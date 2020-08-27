faeed94c25fed4ab85cebbd1a8b9ddfe2a189b8b
Gabriel-Lops <69974225+Gabriel-Lops@users.noreply.github.com>
2020-08-27 10:22:18 -0300

    Create programa01.py

---
+++ LTP2/Pratica02/programa01.py
@@ -0,0 +1,8 @@
+#Função para escrever na tela: print()
+print("Olá Mundo!")
+#Função de leitura de teclado: Input()
+nomte = input("Digite seu nome:")
+'''
+Isso é um bloco de comentários
+'''
+print("Bom dia",nome,"!")
